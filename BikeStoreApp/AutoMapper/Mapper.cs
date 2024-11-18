d916e8e557be3a6996a56f1651a3e5fba8665159
spoorthybalagar <balagarspoorthy@gmail.com>
2024-11-18 17:36:25 +0530

    Mapper added

---
+++ BikeStoreApp/AutoMapper/Mapper.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BikeStoreApp.Dto;
+using BikeStoreApp.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BikeStoreApp.AutoMapper
+{
+    public class Mapper : Profile
+    {
+
+        public Mapper()
+        {
+            CreateMap<CreateCategoryDto, Category>();
+            CreateMap<UpdateCategoryDto, Category>();
+            CreateMap<Category, CategoryDto>();
+
+            // Brand Mapping
+            CreateMap<CreateBrandDto, Brand>();
+            CreateMap<UpdateBrandDto, Brand>();
+            CreateMap<Brand, BrandDto>();
+
+            // Product Mapping
+            CreateMap<CreateProductDto, Product>();
+            CreateMap<UpdateProductDto, Product>();
+            CreateMap<Product, ProductDto>();
+            CreateMap<Product, ProductDetailDto>()
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.ProductName));
+        }
+    }
+}
+
+
