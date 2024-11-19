21de00f3e24b55ab2271a82b1efc064092b0e1b7
Sahana137 <sahanagowda1307@gmail.com>
2024-11-19 14:58:07 +0530

    Revert "Reapply "customer""
    This reverts commit 0977d0daec6e4c3ce972972848717feecad2fcc0.

---
+++ BikeStoreApp/Dto/CreateOrderItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BikeStoreApp.Dto
+{
+    public class CreateOrderItemDto
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal ListPrice { get; set; }
+        public decimal Discount { get; set; }
+    }
+
+}
---
+++ BikeStoreApp/Dto/ResponseOrderDto.cs
@@ -0,0 +1,19 @@
+﻿namespace BikeStoreApp.Dto
+{
+    public class ResponseOrderDto
+    {
+        public int OrderId { get; set; }
+        public int? CustomerId { get; set; }
+        public string? CustomerName { get; set; } // Optional, fetched via navigation property
+        public byte OrderStatus { get; set; }
+        public DateOnly OrderDate { get; set; }
+        public DateOnly RequiredDate { get; set; }
+        public DateOnly? ShippedDate { get; set; }
+        public int StoreId { get; set; }
+        public string StoreName { get; set; } // Optional, fetched via navigation property
+        public int StaffId { get; set; }
+        public string StaffName { get; set; } // Optional, fetched via navigation property
+        public List<ResponseOrderItemDto> OrderItems { get; set; } = new List<ResponseOrderItemDto>();
+    }
+
+}
