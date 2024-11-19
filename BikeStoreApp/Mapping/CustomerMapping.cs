0977d0daec6e4c3ce972972848717feecad2fcc0
Sahana137 <sahanagowda1307@gmail.com>
2024-11-19 14:53:41 +0530

    Reapply "customer"
    This reverts commit 4ccfff4087dc7872b43befaf455d15a12b77994e.

---
+++ BikeStoreApp/Mapping/CustomerMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BikeStoreApp.Dto;
+using BikeStoreApp.Models;
+
+namespace BikeStoreApp.Mapping
+{
+    public class CustomerMapping : Profile
+    {
+        public CustomerMapping()
+        {
+            CreateMap<CreateCustomerDto, Customer>().ReverseMap();
+
+            
+            CreateMap<UpdateCustomerDto, Customer>().ReverseMap();
+
+            
+            CreateMap<Customer, ResponseCustomerDto>().ReverseMap();
+
+           
+            
+        }
+    }
+}
