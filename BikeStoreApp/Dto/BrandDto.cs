4ccfff4087dc7872b43befaf455d15a12b77994e
Sahana137 <sahanagowda1307@gmail.com>
2024-11-19 14:50:17 +0530

    Revert "customer"
    This reverts commit 1631f64f1a9737defb2d5b4aae36c52840d3fc5f.

---
+++ BikeStoreApp/Dto/BrandDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BikeStoreApp.Dto
+{
+    public class BrandDto
+    {
+        public int BrandId { get; set; }
+        public string BrandName { get; set; } = string.Empty;
+    }
+
+}
---
+++ BikeStoreApp/Dto/UpdateOrderDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BikeStoreApp.Dto
+{
+    public class UpdateOrderDto
+    {
+        public byte? OrderStatus { get; set; }
+        public DateOnly? RequiredDate { get; set; }
+        public DateOnly? ShippedDate { get; set; }
+        public int? StoreId { get; set; }
+        public int? StaffId { get; set; }
+        public List<UpdateOrderItemDto> OrderItems { get; set; } = new List<UpdateOrderItemDto>();
+    }
+
+}
